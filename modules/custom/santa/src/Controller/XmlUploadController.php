7a30433b0341a8c779e8c9daa4a045f4d0334ca2
Giovanni Venegas <giovanni@vanguardsd.com>
2018-08-03 01:27:10 +0530

    XML

--- modules/custom/santa/src/Controller/XmlUploadController.php
+++ modules/custom/santa/src/Controller/XmlUploadController.php
@@ -16,7 +16,7 @@ class XmlUploadController extends ControllerBase {
    * @return array
    *   A simple renderable array.
    */
-  public function XmlController() {
+  public function content() {
   	$current_user = \Drupal::currentUser();
 		$userRole = $current_user->getRoles();
 		
