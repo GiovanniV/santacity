0eaa9c676a35d35b99026be2d68c254e106825ae
Giovanni Venegas <giovanni@vanguardsd.com>
2018-08-08 17:37:07 +0530

    Fix

--- modules/custom/santa/js/datatables.js
+++ modules/custom/santa/js/datatables.js
@@ -1,30 +1,33 @@
 (function ($, Drupal) {
 	Drupal.behaviors.santadatatables = {
 		attach: function (context, settings) {
-			// Setup - add a text input to each footer cell
-      $('#xml-preview tfoot td').each( function () {
-        var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Filter '+title+'" />' );
-      });
+			$(document).ready(function(){
+				// Setup - add a text input to each footer cell
+				$('#xml-preview tfoot td').each( function () {
+					var title = $(this).text();
+					$(this).html( '<input type="text" placeholder="Filter '+title+'" />' );
+				});
 
-      // DataTable
-      var table = $('#xml-preview').DataTable({
-        "bLengthChange": false,
-        "bFilter": false,
-        "bInfo": false,
-        "bDestroy": true,
-      });
+				// DataTable
+				var table = $('#xml-preview').DataTable({
+					"bLengthChange": false,
+					"bFilter": false,
+					"bInfo": false,
+					"bDestroy": true,
+				});
 
-      // Apply the search
-      table.api().columns().every( function () {
+				// Apply the search
+				table.api().columns().every( function () {
 
-        var that = this;
-        $( 'input', this.footer() ).on( 'keyup change', function () {
-          if ( that.search() !== this.value ) {
-            that.search(this.value).draw();
-          }
-        });
-      });
+					var that = this;
+					$( 'input', this.footer() ).on( 'keyup change', function () {
+						if ( that.search() !== this.value ) {
+							that.search(this.value).draw();
+						}
+					});
+				});
+			});
+		
 		}
 	};
 })(jQuery, Drupal);
