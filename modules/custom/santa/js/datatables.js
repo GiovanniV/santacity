afbd597e7b8e3b0e7e42f0ae8e72aa520436ed67
Giovanni Venegas <giovanni@vanguardsd.com>
2018-08-08 15:10:50 +0530

    Fix

--- modules/custom/santa/js/datatables.js
+++ modules/custom/santa/js/datatables.js
@@ -1,25 +1,7 @@
 (function ($, Drupal) {
-	Drupal.behaviors.myBehavior = {
+	Drupal.behaviors.dataTables = {
 		attach: function (context, settings) {
-			console.log(111);
-			$('#xml-datatable tfoot th').each( function () {
-				var title = $(this).text();
-				$(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-			});
-	 
-			// DataTable
-			var table = $('#xml-datatable').DataTable();
-	 
-			// Apply the search
-			table.columns().every( function () {
-				var that = this;
-				
-				$( 'input', this.footer() ).on( 'keyup change', function () {
-					if ( that.search() !== this.value ) {
-						that.search( this.value ).draw();
-					}
-				});
-			});
+			
 		}
 	};
 })(jQuery, Drupal);
