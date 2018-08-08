d112eeb6c9fdb85bf6b1042b124e1736db61c47c
Giovanni Venegas <giovanni@vanguardsd.com>
2018-08-08 16:14:25 +0530

    Fix

--- modules/custom/santa/js/datatables.js
+++ modules/custom/santa/js/datatables.js
@@ -6,21 +6,20 @@
         var title = $(this).text();
         $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
       });
-   
+
       // DataTable
-      var table = $('#xml-preview').DataTable();
-   
+      var otable = $('#xml-preview').DataTable();
+
       // Apply the search
-      table.columns().every( function () {
+      otable.columns().every( function () {
+
         var that = this;
- 
         $( 'input', this.footer() ).on( 'keyup change', function () {
           if ( that.search() !== this.value ) {
-            that.search( this.value ).draw();
+            that.search(this.value).draw();
           }
         });
       });
-			
 		}
 	};
 })(jQuery, Drupal);
