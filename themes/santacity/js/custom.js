8704b5a6ddd09a97cd93586ac3170babf871f13e
Giovanni Venegas <giovanni@vanguardsd.com>
2018-08-21 14:29:15 +0530

    Fix

--- themes/santacity/js/custom.js
+++ themes/santacity/js/custom.js
@@ -2,6 +2,11 @@
 	Drupal.behaviors.menu = {
 		attach: function (context, settings) {
 			
+			/** Set Header Top **/
+			var admin_toolbar_height = $('.toolbar-menu-administration .toolbar-menu').outerHeight();
+			var toolbar_height = $('#toolbar-bar').outerHeight();
+			$('header').attr('top', admin_toolbar_height + toolbar_height);
+			
 			$(document).ready(function(){
 				$(".nav li.expanded").hover(
 					function(){
@@ -11,6 +16,8 @@
 					}
 				);
 			});
+			
+			
 		
 		}
 	};
