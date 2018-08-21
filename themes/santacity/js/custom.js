82fbf5a271be6a16f6c6816359bdd0ab80a2ba42
Giovanni Venegas <giovanni@vanguardsd.com>
2018-08-21 14:46:19 +0530

    Fix

--- themes/santacity/js/custom.js
+++ themes/santacity/js/custom.js
@@ -5,12 +5,12 @@
 			/** Set Header Top **/
 			var admin_toolbar_outer_height = $('.toolbar-menu-administration .toolbar-menu').outerHeight();
 			var toolbar_outer_height = $('#toolbar-bar').outerHeight();
-			var header_height = admin_toolbar_height + toolbar_height;
+			var header_height = admin_toolbar_outer_height + toolbar_outer_height;
 			$('header').css('top', header_height + 'px');
 			
 			/** Set Page Heading **/
-			var header_height = $('header').outerHeight();
-			var heading_height = admin_toolbar_height + toolbar_height + header_height;
+			var header_outer_height = $('header').outerHeight();
+			var heading_height = admin_toolbar_outer_height + toolbar_outer_height + header_outer_height;
 			$('.region-page-heading').css('top', heading_height + 'px');
 			
 			$(document).ready(function(){
