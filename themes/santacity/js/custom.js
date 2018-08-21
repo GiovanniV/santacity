efb5f4e83b54f018d487c0e2e40bbd852adc51eb
Giovanni Venegas <giovanni@vanguardsd.com>
2018-08-21 14:34:02 +0530

    Fix

--- themes/santacity/js/custom.js
+++ themes/santacity/js/custom.js
@@ -6,7 +6,7 @@
 			var admin_toolbar_height = $('.toolbar-menu-administration .toolbar-menu').outerHeight();
 			var toolbar_height = $('#toolbar-bar').outerHeight();
 			var header_height = admin_toolbar_height + toolbar_height;
-			$('header').attr('top', header_height + 'px');
+			$('header').css('top', header_height + 'px');
 			
 			$(document).ready(function(){
 				$(".nav li.expanded").hover(
