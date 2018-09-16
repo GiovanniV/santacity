d756aee88720fca13a5ca5384de65e80d96b4897
Giovanni Venegas <giovanni@vanguardsd.com>
2018-09-16 23:01:01 +0530

    Fix

--- themes/santacity/js/custom.js
+++ themes/santacity/js/custom.js
@@ -48,34 +48,7 @@
 				
 			});
 			
-			// Dropdown
-			var backdrop = '.dropdown-backdrop'
-			var toggle   = '[data-toggle="dropdown"]'
-			$(document).on('click.bs.dropdown.data-api', function(e) {
-				if (e && e.which === 3) return
-				$(backdrop).remove()
-				$(toggle).each(function () {
-					var $this         = $(this)
-					var $parent       = getParent($this)
-					var relatedTarget = { relatedTarget: this }
-
-					if (!$parent.hasClass('open')) return
-
-					if (e && e.type == 'click' && /input|textarea/i.test(e.target.tagName) && $.contains($parent[0], e.target)) return
-
-					$parent.trigger(e = $.Event('hide.bs.dropdown', relatedTarget))
-
-					if (e.isDefaultPrevented()) return
-
-					$this.attr('aria-expanded', 'false')
-					$parent.removeClass('open').trigger($.Event('hidden.bs.dropdown', relatedTarget))
-				})
-				$(this).addClass('open');
-				$(this).parents('li').addClass('open');
-				$(this).parents('li').parents('li').addClass('open');
-				$(this).parents('li').parents('li').parents('li').addClass('open');
-			});
-				
+			
 		}
 	};
 })(jQuery, Drupal, drupalSettings);
